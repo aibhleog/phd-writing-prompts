af1fefe279e420ec4db5697abc419a39cdba5245
aibhleog <aibhleog@tamu.edu>
2021-06-30 16:32:45 -0500

    adding list & code

---
+++ get-prompt.py
@@ -0,0 +1,23 @@
+'''
+This script generates a random number and pulls a prompt from the 
+"list-of-prompts.txt" file.  The top of that file lists the 
+website that the prompts were pulled from.
+
+Can be turned into an alias to be run from the terminal
+'''
+
+import numpy as np
+import pandas as pd
+
+# reading in list of prompts
+df = pd.read_csv('list-of-prompts.txt',skiprows=4,sep='\n',names=['prompt'])
+
+num = round(np.random.uniform(0,len(df))) # generating random number
+
+print(f'''
+---------------
+WRITING PROMPT:
+---------------
+{df.loc[num,"prompt"]}
+''')
+
